2ed6e7cf3e8a412a309f565aa87c54cfbcec78fb
ManasaSM1 <89134868+ManasaSM1@users.noreply.github.com>
2021-11-19 20:35:11 +0530

    Add files via upload

---
+++ bookingData.js
@@ -0,0 +1,12 @@
+const bookingData = [
+	{
+		customerName: "Manasa",
+		date: "2021-11-19",
+		startTime: "01",
+		endTime: "10",
+		roomId: 1,
+		status: "confirm",
+	},
+];
+
+module.exports = bookingData;
---
+++ hallData.js
@@ -0,0 +1,33 @@
+const hallData = [
+	{
+		roomId: 1,
+		roomName: "a1",
+		numberOfSeats: 100,
+		amenities: ["fan", "ac", "generator", "fancy lighting"],
+		chargePerHourInRupee: 4000,
+	},
+	{
+		roomId: 2,
+		roomName: "a2",
+		numberOfSeats: 50,
+		amenities: ["fan", "genrator"],
+		chargePerHourInRupee: 3000,
+	},
+	{
+		roomId: 3,
+		roomName: "a3",
+		numberOfSeats: 500,
+		amenities: ["fan", "genrator", "ac", "sound system"],
+		chargePerHourInRupee: 8000,
+	},
+
+	{
+		roomId: 4,
+		roomName: "a4",
+		numberOfSeats: 200,
+		amenities: ["fan", "genrator", "ac", "sound system"],
+		chargePerHourInRupee: 6000,
+	},
+];
+
+module.exports = hallData;
